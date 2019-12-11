9630d4bba347bb6b201483f7355063379526c26e
Reni <reniarahim@gmail.com>
2019-12-10 19:00:57 -0700

    Test

---
+++ src/Fibinocci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fibinocci {
+
+    public static int fib(int i){
+        if (i == 0){
+            return 1;
+        }
+        else if (i == 1){
+            return 1;
+        }
+        else {
+            return fib(i-1) + fib(i-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the Number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(fib(num));
+    }
+}
